fb2e44f6fa9ad5130cb4eab1eda38ec43e5473c7
PhilipRobertsson <phiro138@student.liu.se>
2025-02-23 17:54:00 +0100

    Current database added and processed

--- databaseProcessing.m
+++ databaseProcessing.m
@@ -15,9 +15,44 @@ theFilesPNG = dir(filePatternPNG);
 theFilesJPG = dir(filePatternJPG);
 theFilesJPEG = dir(filePatternJPEG);
 
-theFiles = [theFilesPNG,theFilesJPG, theFilesJPEG];
+totLength = length(theFilesJPEG) + length(theFilesJPG) + length(theFilesPNG);
+theFiles = struct("name",[],"folder",[],"date",[],"bytes",[],"isdir",[],"datenum",[]);
+structrow = 0;
 
-croppedImages = {};
+for k = 1 : totLength
+
+    if k <= length(theFilesPNG)
+        structrow = structrow + 1;
+        theFiles(structrow).name = theFilesPNG(k,1).name;
+        theFiles(structrow).folder = theFilesPNG(k,1).folder;
+        theFiles(structrow).date = theFilesPNG(k,1).date;
+        theFiles(structrow).bytes = theFilesPNG(k,1).bytes;
+        theFiles(structrow).isdir = theFilesPNG(k,1).isdir;
+        theFiles(structrow).datenum = theFilesPNG(k,1).datenum;
+    end
+
+    if k <= length(theFilesJPEG)
+        structrow = structrow + 1;
+        theFiles(structrow).name = theFilesJPEG(k,1).name;
+        theFiles(structrow).folder = theFilesJPEG(k,1).folder;
+        theFiles(structrow).date = theFilesJPEG(k,1).date;
+        theFiles(structrow).bytes = theFilesJPEG(k,1).bytes;
+        theFiles(structrow).isdir = theFilesJPEG(k,1).isdir;
+        theFiles(structrow).datenum = theFilesJPEG(k,1).datenum;
+    end
+
+    if k <= length(theFilesJPG)
+        structrow = structrow + 1;
+        theFiles(structrow).name = theFilesJPG(k,1).name;
+        theFiles(structrow).folder = theFilesJPG(k,1).folder;
+        theFiles(structrow).date = theFilesJPG(k,1).date;
+        theFiles(structrow).bytes = theFilesJPG(k,1).bytes;
+        theFiles(structrow).isdir = theFilesJPG(k,1).isdir;
+        theFiles(structrow).datenum = theFilesJPG(k,1).datenum;
+    end
+end
+
+%croppedImages = {};
 scaledImages = {};
 
 for k = 1 : length(theFiles)
@@ -25,23 +60,19 @@ for k = 1 : length(theFiles)
     fullFileName = fullfile(theFiles(k).folder, baseFileName);
     IMG = imread(append(filePathRaw,baseFileName));
 
-    
     imgHeight = size(IMG,1);
     imgWidth = size(IMG,2);
     
     if(imgHeight > imgWidth)
-        IMGcrop = imcrop(IMG, [floor((imgWidth-imgHeight)/2) floor((imgWidth-imgHeight)/2) floor((imgWidth+imgHeight)/2) floor((imgWidth+imgHeight)/2)]);
         IMGres=imresize(IMG,[imgWidth imgWidth],'bicubic');
     elseif(imgHeight < imgWidth)
-        IMGcrop = imcrop(IMG, [floor((imgHeight-imgWidth)/2) floor((imgHeight-imgWidth)/2) floor((imgHeight+imgWidth)/2) floor((imgHeight+imgWidth)/2)]);
         IMGres=imresize(IMG,[imgHeight imgHeight],'bicubic');
+    else
+        IMGres = IMG;
     end
-
-    croppedImages = [croppedImages, IMGcrop];
+    fullFileName = fullfile(filePathProc, baseFileName);
+    imwrite(IMGres, fullFileName);
     scaledImages = [scaledImages, IMGres];
 end
 
-figure;
-montage(croppedImages, "size", [1 3]);
-figure;
-montage(scaledImages, "size", [1 3]);
+montage(scaledImages);
