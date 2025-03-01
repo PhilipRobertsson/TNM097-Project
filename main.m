95976113ab92798fe7fdd34b1116b8373cd30795
PhilipRobertsson <phiro138@student.liu.se>
2025-03-01 12:34:00 +0100

    Image loading

--- main.m
+++ main.m
@@ -1,3 +1,17 @@
 % Add folders and subfolders to path
 addpath("functions\") % Functions
 addpath("images\processed\") % Processed Images
+
+% Image loading
+filterspec = {'*.jpg;*.tif;*.png;*.gif','All Image Files'};
+[file,path] = uigetfile(filterspec);
+imgInput = imread(fullfile(path,file));
+
+imshow(imgInput);
+
+% Divide image into smaller segments
+segmentInWidth = 10;
+[rows, columns, numberOfColorBands] = size(rgbImage)
+segmentSize = floor(rows/10); % Height and width
+
+
