628c41ca51ae89650076e91affca6631f80dd581
PhilipRobertsson <phiro138@student.liu.se>
2025-03-01 13:01:00 +0100

    Segmentation works

--- main.m
+++ main.m
@@ -10,8 +10,41 @@ imgInput = imread(fullfile(path,file));
 imshow(imgInput);
 
 % Divide image into smaller segments
-segmentInWidth = 10;
-[rows, columns, numberOfColorBands] = size(rgbImage)
-segmentSize = floor(rows/10); % Height and width
+segmentSizeR = 250; % R, rows
+segmentSizeC = 250; % C, columns
+[rows, columns, channels] = size(imgInput);
 
+% Number of segments for rows
+wholeSegmentRows = floor(rows / segmentSizeR);
+% Row size for every segment, remaining row size for edge segment
+SegmentVectorR = [segmentSizeR * ones(1, wholeSegmentRows), rem(rows, segmentSizeR)];
+
+% Number of segments for columns
+wholeSegmentCols = floor(columns / segmentSizeC);
+% Column size for every segment, remaining column size for edge segment
+SegmentVectorC = [segmentSizeC * ones(1, wholeSegmentCols), rem(columns, segmentSizeC)];
+
+% Split image into cells based on previous calculations
+if channels > 1
+    % Colour.
+    imgCell = mat2cell(imgInput, SegmentVectorR, SegmentVectorC, channels);
+else
+    % Grayscale
+    imgCell = mat2cell(imgInput, SegmentVectorR, SegmentVectorC);
+end
+
+% Index for subplot
+plotIndex = 1;
+% Number of subplots needed, rows, columns
+[numPlotsR, numPlotsC] = size(imgCell);
+for r = 1 : numPlotsR
+    for c = 1 : numPlotsC
+        % Subplot used to draw image split up in segments
+        subplot(numPlotsR, numPlotsC, plotIndex);
+        % Draw specified cell
+        imshow(imgCell{r,c}); 
+        % Increment the subplot to the next location.
+        plotIndex = plotIndex + 1;
+    end
+end
 
